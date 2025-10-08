1cf238880628f129bf26e823e7a7bfc479c2c4d2
dyannadle <dyannadle05@gmail.com>
2025-10-08 22:49:59 +0530

    Auto commit on 10/08/2025 22:49:59

---
+++ Java/Java/chapter2.java
@@ -0,0 +1,61 @@
+
+// import java.util.Scanner;
+
+
+//import java.util.Scanner;
+
+//what will be the output of the following code?
+
+// public class chapter2 {
+//     public static void main(String[] args) {
+//         float a= 7/4.0f * 9/2.0f;
+//         System.out.println(a);
+//     }
+// }
+
+
+//write a java progaram to encrypt a garde by adding 8 to it. Decrypt it to show the correct grade.
+
+// //encryption
+// public class chapter2 {
+
+// public static void main (String [] args){
+//     char grade = 'B';
+//     grade = (char)(grade + 8);
+//     System.out.println(grade);  
+ 
+
+
+// //decryption
+//     grade = (char)(grade - 8);  
+//     System.out.println(grade);
+// }
+// }
+
+
+// Question 3
+//use comparison operators to find out whether a given number is greater than the user entered number or not    
+
+
+// public class chapter2{
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner (System.in);
+//         int a= sc.nextInt();
+//         System.out.println(a>8);
+//     }
+
+// }
+
+
+//find the value of the following expression
+//int x=7;
+// int a=7*49/7+35/7;
+public class chapter2{
+    public static void main(String[] args) {
+        //  Scanner sc = new Scanner(System.in);
+         
+int a=7*49/7+35/7;
+System.out.println(a);
+    }
+}
+
