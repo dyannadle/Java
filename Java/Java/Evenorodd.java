313170b01cd6fede32526e24c281cfe73fe47f6b
dyannadle <dyannadle05@gmail.com>
2025-10-10 22:48:49 +0530

    Auto commit on 10/10/2025 22:48:49

--- Java/Java/Evenorodd.java
+++ Java/Java/Evenorodd.java
@@ -3,8 +3,9 @@ import java.util.Scanner;
 public class Evenorodd{
     public static void main(String []args){
 Scanner sc = new Scanner(System.in);
-int num = sc.nextInt();
 System.out.println("Enter a number:");
+int num = sc.nextInt();
+
 if (num % 2==0){
     System.out.println("The number is even");
 }
