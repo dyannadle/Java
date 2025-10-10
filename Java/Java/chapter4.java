a6e55f4b4cd14134a5499479fbf840d1f0ed8679
Mr.Deepak Yannadle <104290668+dyannadle@users.noreply.github.com>
2025-10-10 16:45:43 +0530

    Update chapter4.java

--- Java/Java/chapter4.java
+++ Java/Java/chapter4.java
@@ -1,1 +1,84 @@
+//Write a program to find out whether a student is pass or fail; if it requires total 40% and at least 33% in each subject to pass. Assume 3 subjects and take marks as an input from the user.
+import java.util.Scanner;
+public class chapter4{
+  public static void main(String[]args){
+    Scanner sc = new Scanner (System.in);
+      byte m1,m2,m3;
+    System.out.println("Enter marks of maths :");
+    m1= sc.nextByte();
+        System.out.println("Enter marks of Geography :");
+    m2= sc.nextByte();
+        System.out.println("Enter marks of Science-I :");
+    m3= sc.nextByte();
+
+    float avg= (m1+m2+m3)/3.0f;
+    if (avg>=40 && m1>=33  && m2>=33 && m3>=33){
+      System.out.println("Congulations App pass ho gaye ho!");
+    }
+    else {
+           System.out.println(":( Better luck next time !");
+    }
+  }
+
+}
+
+
+
+
+// 3 . Problem Question:
+// //"Calculate income tax paid by an employee to the Government as per the slabs mentioned below:"
+// Income Tax Slabs:
+// Income Slab: 2.5L - 5.0L
+// Tax: 5%
+// Income Slab: 5.0L - 10.0L
+// Tax: 20%
+// Income Slab: Above 10.0L
+// Tax: 30%
+//Note that there is no tax below 2.5l . Take input amount as an input from the user . 
+
+
+
+// 4. Question 
+
+import java.util.Scanner;
+public class Main{
+  public static void main(String[]args){
+    Scanner sc = new Scanner (System.in);
+    System.out.println("Enter a number (1-7) for the day of the week:");
+    int day= sc.nextInt();
+
+    switch(day){
+      case 1:
+        System.out.println("monday");
+             break;
+      case 2:
+        System.out.println("tuesday");
+             break;
+            case 3 : 
+        System.out.println("wednesday");
+             break;
+      case 4:
+        System.out.println("thursday");
+             break;
+      case 5:
+        System.out.println("friday");
+             break;
+            case 6: 
+        System.out.println("saturday");
+             break;
+            case 7 : 
+        System.out.println("sunday");
+             break;
+      default:
+        System.out.println("Enter a vaild number");
+    }
+  }
+}
+   
+
+
+
+
+
+
 
