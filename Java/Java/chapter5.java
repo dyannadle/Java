63d7e84d8aab619c03f613c1fb68384670e1a3d7
dyannadle <dyannadle05@gmail.com>
2025-10-12 19:42:38 +0530

    Auto commit on 10/12/2025 19:42:37

--- Java/Java/chapter5.java
+++ Java/Java/chapter5.java
@@ -8,12 +8,17 @@
 //     }
 // }
 
+
+// while loop
  public class chapter5{
     public static void main(String[]args){
         int i=1;
         while(i<=9){
             System.out.println(i);
             i++;
+        }
+        while(true){
+            System.out.println("I am an infinite loop");
         }
     }
  }
