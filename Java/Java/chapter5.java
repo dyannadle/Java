2a011d01e0e3cf578b708efc0bb656799bb45988
dyannadle <dyannadle05@gmail.com>
2025-10-14 22:18:39 +0530

    Auto commit on 10/14/2025 22:18:39

--- Java/Java/chapter5.java
+++ Java/Java/chapter5.java
@@ -118,15 +118,22 @@
 
 // FOR LOOP
 
+// public class chapter5{
+//     public static void main(String[]args){
+//         for(int i=1;i<=10; i++){
+//             System.out.println(i);
+//         }
+//     }
+// }
+
 public class chapter5{
     public static void main(String[]args){
-        for(int i=1;i<=10; i++){
-            System.out.println(i);
+   int i=10;
+        for(int i=0; i<n; i++){
+            System.out.println(2*i+1);
         }
     }
 }
-
-
 
 
 
