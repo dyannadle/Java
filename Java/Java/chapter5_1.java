a1adb8574f3c7cc5f7c63db247ebc90efdea114c
dyannadle <dyannadle05@gmail.com>
2025-10-15 22:01:56 +0530

    Auto commit on 10/15/2025 22:01:56

--- Java/Java/chapter5_1.java
+++ Java/Java/chapter5_1.java
@@ -70,21 +70,21 @@
 
 
 //write a program to print sum of odd numbers
-import java.util.Scanner;    
-public class chapter5_1 {
-    public static void main(String[]args){
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter a number :");
-        int n= sc.nextInt();
-        int sum=0;
-        for(int i=1; i<=n; i++){
-            if(i%2!=0){
-                sum=sum+i;
-            }
-        }
-        System.out.println("The sum of odd numbers is :" + sum);
-    }
-}
+// import java.util.Scanner;    
+// public class chapter5_1 {
+//     public static void main(String[]args){
+//         Scanner sc = new Scanner(System.in);
+//         System.out.println("Enter a number :");
+//         int n= sc.nextInt();
+//         int sum=0;
+//         for(int i=1; i<=n; i++){
+//             if(i%2!=0){
+//                 sum=sum+i;
+//             }
+//         }
+//         System.out.println("The sum of odd numbers is :" + sum);
+//     }
+// }
 
 // write a program to print first n natural numbers in reverse order
 // import java.util.Scanner;
@@ -100,3 +100,16 @@ public class chapter5_1 {
 // }
 
 
+//write a progaram to print multiplication table of a given number
+import java.util.Scanner;
+public class chapter5_1 {
+    public static void main(String[]args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a number :");// prompt user for input
+        int n= sc.nextInt();// input number
+        System.out.println("Multiplication table of " + n + " is :");
+        for(int i=1; i<=10; i++){// loop from 1 to 10
+            System.out.println(n + " * " + i + " = " + (n*i));// print n*i
+        }
+    }
+}
