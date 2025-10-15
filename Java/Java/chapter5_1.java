8457061c47983feda26f60db4343b0236a54e0a4
dyannadle <dyannadle05@gmail.com>
2025-10-15 21:46:29 +0530

    Auto commit on 10/15/2025 21:46:29

--- Java/Java/chapter5_1.java
+++ Java/Java/chapter5_1.java
@@ -25,5 +25,16 @@
 
 //write a program to print first n even numbers
 
+import java.util.Scanner;
+public class chapter5_1 {
+    public static void main(String[]args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a number :");
+        int n= sc.nextInt();
+        for(int i=1; i<=n; i++){
+            System.out.println(2*i);
+        }
+    }
+}
 
 
