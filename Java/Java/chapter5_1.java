1d86adb5eba4b323d06cdb75298ebc3cd2f8d048
dyannadle <dyannadle05@gmail.com>
2025-10-15 22:27:45 +0530

    Auto commit on 10/15/2025 22:27:45

--- Java/Java/chapter5_1.java
+++ Java/Java/chapter5_1.java
@@ -224,15 +224,31 @@
 //zero or more times
 //once or more times
 
+// import java.util.Scanner;
+//     public class chapter5_1 {   
+//         public static void main(String[]args){
+//             Scanner sc = new Scanner(System.in);
+//             int n=5;
+//             int i=1;
+//             do{
+//                 System.out.println(i);
+//                 i++;
+//             }while(i<=n);
+//         }
+//     }
+
+
+
+
+
+    //repeat 2 using for loop
 import java.util.Scanner;
-    public class chapter5_1 {   
-        public static void main(String[]args){
-            Scanner sc = new Scanner(System.in);
-            int n=5;
-            int i=1;
-            do{
-                System.out.println(i);
-                i++;
-            }while(i<=n);
-        }
+public class chapter5_1 {    
+    public static void main(String[]args){
+        Scanner sc = new Scanner(System.in); 
+        int n=5;
+        for(int i=1; i<=n; i++){     
+            System.out.println(2*i);
+        }    
     }
+}
