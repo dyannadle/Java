ff5770956b8f64d12f71bed6dd9b4adfd2057600
dyannadle <dyannadle05@gmail.com>
2025-10-15 22:03:00 +0530

    Auto commit on 10/15/2025 22:03:00

--- Java/Java/chapter5_1.java
+++ Java/Java/chapter5_1.java
@@ -101,15 +101,32 @@
 
 
 //write a progaram to print multiplication table of a given number
-import java.util.Scanner;
+// import java.util.Scanner;
+// public class chapter5_1 {
+//     public static void main(String[]args){
+//         Scanner sc = new Scanner(System.in);
+//         System.out.println("Enter a number :");// prompt user for input
+//         int n= sc.nextInt();// input number
+//         System.out.println("Multiplication table of " + n + " is :");
+//         for(int i=1; i<=10; i++){// loop from 1 to 10
+//             System.out.println(n + " * " + i + " = " + (n*i));// print n*i
+//         }
+//     }
+// }
+
+
+
+//write a program to print factorial of a given number
+import java.util.Scanner;   
 public class chapter5_1 {
     public static void main(String[]args){
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter a number :");// prompt user for input
-        int n= sc.nextInt();// input number
-        System.out.println("Multiplication table of " + n + " is :");
-        for(int i=1; i<=10; i++){// loop from 1 to 10
-            System.out.println(n + " * " + i + " = " + (n*i));// print n*i
+        System.out.println("Enter a number :");
+        int n= sc.nextInt();
+        int fact=1;
+        for(int i=n; i>0; i--){
+            fact=fact*i;
         }
+        System.out.println("The factorial of " + n + " is :" + fact);
     }
 }
