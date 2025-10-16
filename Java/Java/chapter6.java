914f7fa598fbe6b7b4263902b35db7e08c665206
dyannadle <dyannadle05@gmail.com>
2025-10-16 22:11:12 +0530

    Auto commit on 10/16/2025 22:11:12

--- Java/Java/chapter6.java
+++ Java/Java/chapter6.java
@@ -1,14 +1,30 @@
+// public class chapter6 {
+//     public static void main(String[]args){
+//         int [] marks  = new int[7];
+//         marks[0]=97;
+//         marks[1]=98;
+//         marks[2]=95;
+//         marks[3]=99;    
+//         marks[4]=96;
+//         marks[5]=100;
+//         System.out.println(marks[2]);
+//         System.out.println(marks.length);
+//     }
+    
+// }
+
+
 public class chapter6 {
     public static void main(String[]args){
-        int [] marks  = new int[7];
-        marks[0]=97;
+        int [] marks ;
+        marks = new int[6];
+          marks[0]=97;
         marks[1]=98;
         marks[2]=95;
         marks[3]=99;    
         marks[4]=96;
         marks[5]=100;
-        System.out.println(marks[2]);
-        System.out.println(marks.length);
+ System.out.println(marks[2]);
+        System.out.println(marks.length );
     }
-    
 }
