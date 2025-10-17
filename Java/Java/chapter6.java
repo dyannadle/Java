5c1d9124ee823eb940c97d74ecd7c05b414d1a50
Mr.Deepak Yannadle <104290668+dyannadle@users.noreply.github.com>
2025-10-17 12:40:56 +0530

    Implement array display with for loops
    Added functionality to display array elements using loops.

--- Java/Java/chapter6.java
+++ Java/Java/chapter6.java
@@ -57,9 +57,9 @@ public class chapter6 {
 public class chapter6{
     public static void main(String [] args ){
            String[] student={"The", "secret", "is" ,"not", "to", "find", "the", "meaning" ,"of", "life", "but", "to", "use", "your", "life", "to", "make", "things", "that" ,"are ","meaningful"};
-        System.out.println(student[0]);
-    System.out.println(student[1]);     
-     System.out.println(student[2]);
+         System.out.println(student[0]);
+         System.out.println(student[1]);     
+         System.out.println(student[2]);
          System.out.println(student[3]);
          System.out.println(student[4]);
          System.out.println(student[5]);
@@ -69,7 +69,7 @@ public class chapter6{
          System.out.println(student[9]);
          System.out.println(student[10]);
          System.out.println(student[11]);
-        System.out.println(student[12]);
+         System.out.println(student[12]);
          System.out.println(student[13]);
          System.out.println(student[14]);
          System.out.println(student[15]);
@@ -78,12 +78,58 @@ public class chapter6{
          System.out.println(student[18]);
          System.out.println(student[19]);
          System.out.println(student[20]);
-       
-
-
-
         
          System.out.println(student.length);
     }
 }
 
+
+
+
+
+//displaying the aaray (for loop)
+
+public class chapter6 {
+    public static void main(String[]args){
+        int [] marks ;
+        marks = new int[6];
+          marks[0]=97;
+        marks[1]=98;
+        marks[2]=95;
+        marks[3]=99;    
+        marks[4]=96;
+        marks[5]=100;
+System.out.println("Printing using the loop ");
+for(int i=0;i<marks.length; i++){
+    System.out.println(marks[i]);
+}
+}
+    }
+
+
+
+
+//displaying the aaray in reverse (for loop)
+public class chapter6 {
+    public static void main(String[]args){
+        int [] marks ;
+        marks = new int[6];
+          marks[0]=97;
+        marks[1]=98;
+        marks[2]=95;
+        marks[3]=99;    
+        marks[4]=96;
+        marks[5]=100;
+System.out.println("Printing using the loop in reverse");
+for(int i=marks.length-1 ; i>=0; i--){
+    System.out.println(marks[i]);
+}
+}
+    }
+
+
+
+
+
+
+    
