9d991f206f37d4109243022e2cf2751037649464
Mr.Deepak Yannadle <104290668+dyannadle@users.noreply.github.com>
2025-10-17 12:21:16 +0530

    Refactor chapter6 class with new student array
    Updated the chapter6 class to include a new main method that prints elements of a longer student array.

--- Java/Java/chapter6.java
+++ Java/Java/chapter6.java
@@ -38,6 +38,10 @@ public class chapter6{
     }
 }
 
+
+
+
+
 public class chapter6 {
     public static void main(String[]args ){
         String[] student={"1","2","3","4"};
@@ -45,3 +49,41 @@ public class chapter6 {
     System.out.println(student.length);
     }
 }
+
+
+
+
+
+public class chapter6{
+    public static void main(String [] args ){
+           String[] student={"The", "secret", "is" ,"not", "to", "find", "the", "meaning" ,"of", "life", "but", "to", "use", "your", "life", "to", "make", "things", "that" ,"are ","meaningful"};
+        System.out.println(student[0]);
+    System.out.println(student[1]);     
+     System.out.println(student[2]);
+         System.out.println(student[3]);
+         System.out.println(student[4]);
+         System.out.println(student[5]);
+         System.out.println(student[6]);
+         System.out.println(student[7]);
+         System.out.println(student[8]);
+         System.out.println(student[9]);
+         System.out.println(student[10]);
+         System.out.println(student[11]);
+        System.out.println(student[12]);
+         System.out.println(student[13]);
+         System.out.println(student[14]);
+         System.out.println(student[15]);
+         System.out.println(student[16]);
+         System.out.println(student[17]);
+         System.out.println(student[18]);
+         System.out.println(student[19]);
+         System.out.println(student[20]);
+       
+
+
+
+        
+         System.out.println(student.length);
+    }
+}
+
