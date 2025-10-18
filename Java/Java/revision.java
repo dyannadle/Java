56bd8378672639dfb001bd88df2ec62279b29e96
dyannadle <dyannadle05@gmail.com>
2025-10-18 14:13:25 +0530

    Auto commit on 10/18/2025 14:13:24

--- Java/Java/revision.java
+++ Java/Java/revision.java
@@ -1,7 +1,22 @@
+
+// Write a Java program to print “Hello, Java World!” on the console.
+
+
+// public class revision {
+
+//     public static void main(String[] args) {
+//         System.out.println("Hello, Java world!");
+//         }
+//    }
+    
+
+
 public class revision {
     public static void main(String[] args) {
-        System.out.println("Hello, Java world!");
+        char a= 'deepak';
+        char b = 'kumar';
+        char c = a+b;
+        System.out.println(c);
         }
-    }
-    
+   }
 
