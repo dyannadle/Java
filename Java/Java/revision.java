37792f86c1d54c90ee964ba7ba02b756f23c0c57
dyannadle <dyannadle05@gmail.com>
2025-10-18 14:10:17 +0530

    Auto commit on 10/18/2025 14:10:17

--- Java/Java/revision.java
+++ Java/Java/revision.java
@@ -1,7 +1,6 @@
 public class revision {
     public static void main(String[] args) {
-char hello = "hello world";
-        System.out.println(hello);
+        System.out.println("Hello, Java world!");
         }
     }
     
