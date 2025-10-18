0d62ce3d7adf72a3955bb41eff0fcc5147ed35be
dyannadle <dyannadle05@gmail.com>
2025-10-18 14:40:13 +0530

    Auto commit on 10/18/2025 14:40:13

--- Java/Java/revision.java
+++ Java/Java/revision.java
@@ -10,13 +10,26 @@
 //    }
     
 
+//Write a program to print your name, age, and city using three System.out.println() statements.
+
+// public class revision {
+//     public static void main(String[] args) {
+//        String name= "Deepak";
+//        int age =24;
+//        String city = "New Delhi";
+//         System.out.println(name + " is " + age+ " years old and he  lives in " + city);
+//         }
+//    }
+
 
 public class revision {
     public static void main(String[] args) {
        String name= "Deepak";
        int age =24;
        String city = "New Delhi";
-        System.out.println(name + " is " + age+ "years old and he  lives in" + city);
+    double marks =94.5;
+    boolean isBoy = true;
+    float cgpa = 8.9f;
+       System.out.println("Name: " + name + "\nAge: " + age+ "\nCity: " + city + "\nmarks in double: " + age + "\nIs Boy: " + isBoy + "\nCGPA: " + cgpa);    
         }
    }
-
